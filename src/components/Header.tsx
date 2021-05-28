81d479e453692ec360e0987e5693e5fab97db69d
dvargas42 <dvargas.eng@gmail.com>
2021-05-28 20:28:04 -0300

    refactor: create SearchBox component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { Box, Flex, Input, Icon, Text, HStack, Avatar } from '@chakra-ui/react'
 import { RiNotificationLine, RiSearchLine, RiUserAddLine} from 'react-icons/ri'
+
 import { NotificationsNav } from './Header/NotificationNav'
 import { Profile } from './Header/Profile'
+import { SearchBox } from './Header/SearchBox'
 
 export function Header() {
   return(
@@ -25,31 +27,7 @@ export function Header() {
         <Text as="span" marginLeft="1" color="pink.500">.</Text>
       </Text>
 
-      <Flex
-        as="label"
-        flex="1"
-        paddingY="4"
-        paddingX="8"
-        marginLeft="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        backgroundColor="gray.800"
-        borderRadius="full"
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          paddingX="4"
-          marginRight="4"
-          placeholder="Buscar na plataforma"
-          _placeholder={{
-            color: 'gray.400',
-          }}
-        />
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
+      <SearchBox />
 
       <Flex 
       alignItems="center"
