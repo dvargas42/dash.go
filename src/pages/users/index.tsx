14e8029bd0845a007dc983c0cb071d7ea43328ec
dvargas42 <dvargas.eng@gmail.com>
2021-05-28 18:18:29 -0300

    :tada: intial commit

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,143 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex width="100%" maxWidth={1480} marginY="6" marginX="auto" paddingX="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding="8">
+          <Flex marginBottom="8" justifyContent="space-between" alignItems="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th width="8" paddingX="6" color="gray.300">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr color="white">
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Vargas</Text>
+                    <Text fontSize="sm" color="gray.300">dvargas.eng@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>28 de Maio, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Vargas</Text>
+                    <Text fontSize="sm" color="gray.300">dvargas.eng@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>28 de Maio, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Vargas</Text>
+                    <Text fontSize="sm" color="gray.300">dvargas.eng@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>28 de Maio, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Vargas</Text>
+                    <Text fontSize="sm" color="gray.300">dvargas.eng@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>28 de Maio, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
