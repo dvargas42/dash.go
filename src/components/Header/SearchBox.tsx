61f65fcb7074100b2dbd2cff0c8848d10233e532
dvargas42 <dvargas.eng@gmail.com>
2021-05-29 05:37:47 -0300

    feat: implement responsivity in width and margin

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,15 +1,20 @@
-import { Flex, Icon, Input } from "@chakra-ui/react";
+import { Flex, Icon, Input, useBreakpointValue } from "@chakra-ui/react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+  const isWideVersion = useBreakpointValue({
+    base: 655,
+    lg: 400,
+  })
+
   return (
     <Flex
       as="label"
       flex="1"
       paddingY="4"
       paddingX="8"
-      marginLeft="6"
-      maxWidth={400}
+      marginLeft= {["6", "0"]}
+      maxWidth={isWideVersion}
       alignSelf="center"
       color="gray.200"
       position="relative"
