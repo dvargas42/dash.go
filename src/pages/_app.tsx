030b1590db6917a77d060cf4d513238bea7b239c
dvargas42 <dvargas.eng@gmail.com>
2021-05-29 13:04:37 -0300

    feat: implement sidebardrawer provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { SiderbarDrawerProvider } from '../hooks/SidebarDrawerHook'
+
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <SiderbarDrawerProvider>
+        <Component {...pageProps} />
+      </SiderbarDrawerProvider>
     </ChakraProvider>
     )
 }
