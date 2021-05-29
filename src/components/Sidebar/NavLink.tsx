9f6ab5865899a74e517ffdfc716a6d7a5025eb42
dvargas42 <dvargas.eng@gmail.com>
2021-05-29 03:57:02 -0300

    feat: extend LinkProps

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,14 +1,14 @@
 import { ElementType } from "react";
-import { Icon, Link, Text } from "@chakra-ui/react";
+import { Icon, Link, Text, LinkProps } from "@chakra-ui/react";
 
-interface NavLinkProps {
+interface NavLinkProps extends LinkProps {
   icon: ElementType;
   children: string;
 }
 
-export function NavLink({ icon, children }: NavLinkProps) {
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" alignItems="center">
+    <Link display="flex" alignItems="center" {...rest}>
       <Icon as={icon} fontSize="20"/>
       <Text marginLeft="4" fontWeight="medium">{children}</Text>
     </Link>
