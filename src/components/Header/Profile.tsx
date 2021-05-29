e5410fe89056c961931d3bca78e4136ba6cbeb52
dvargas42 <dvargas.eng@gmail.com>
2021-05-29 05:40:25 -0300

    feat: implement visibility in ProfileData

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex alignItems="center">
-      <Box marginRight="4" textAlign="right">
-        <Text>Daniel Vargas</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          dvargas.eng@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>Daniel Vargas</Text>
+          <Text color="gray.300" fontSize="small">
+            dvargas.eng@gmail.com
+          </Text>
+        </Box>
+      )}
     
       <Avatar size="md" name="Daniel Vargas" src="https://github.com/dvargas42.png" />
     </Flex>
