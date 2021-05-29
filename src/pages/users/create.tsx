ae11d7c3cb10ae751080a783f1833b4a0fe2569c
dvargas42 <dvargas.eng@gmail.com>
2021-05-29 15:16:33 -0300

    feat: implement responsivity logic

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,42 +1,62 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  useBreakpointValue,
+  VStack
+} from "@chakra-ui/react";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Input } from "../../components/Form/Input"
+import { SearchBox } from "../../components/Header/SearchBox";
 
 export default function UserCreate() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+  
   return (
     <Box>
       <Header />
 
-      <Flex width="100%" maxWidth={1480} marginY="6" marginX="auto" paddingX="6">
-        <Sidebar />
+        <Flex width="100%" maxWidth={1480} marginY="6" marginX="auto" paddingX="6">
+          <Sidebar />
 
-        <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding="8">
-          <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
+          <Box flex="1" flexDirection="column">
+            {!isWideVersion && <SearchBox marginY="4"/>}
 
-          <Divider marginY="6" borderColor="gray.700" />
+            <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding={["6", "8"]}>
+              <Heading size={ isWideVersion ? "lg" : "md"} fontWeight="normal">Criar Usuário</Heading>
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
-              <Input name="name" label="Nome Completo" />
-              <Input name="email" label="E-mail" type="email" />
-            </SimpleGrid>
+              <Divider marginY="6" borderColor="gray.700" />
 
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
-              <Input name="password" label="Senha" type="password" />
-              <Input name="password" label="Confimação de senha" type="password" />
-            </SimpleGrid>
-          </VStack>
+              <VStack spacing={["6", "8"]}>
+                <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} width="100%">
+                  <Input name="name" label="Nome Completo" />
+                  <Input name="email" label="E-mail" type="email" />
+                </SimpleGrid>
 
-          <Flex marginTop="8" justifyContent="flex-end">
-            <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
-              <Button colorScheme="pink">Salvar</Button>
-            </HStack>
-          </Flex>
-        </Box>
-      </Flex>
+                <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} width="100%">
+                  <Input name="password" label="Senha" type="password" />
+                  <Input name="password" label="Confimação de senha" type="password" />
+                </SimpleGrid>
+              </VStack>
+
+              <Flex marginTop="8" justifyContent="flex-end">
+                <HStack spacing="4">
+                  <Button colorScheme="whiteAlpha">Cancelar</Button>
+                  <Button colorScheme="pink">Salvar</Button>
+                </HStack>
+              </Flex>
+            </Box>
+          </Box>
+        </Flex>
     </Box>
   )
 }
