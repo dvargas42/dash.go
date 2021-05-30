5611726560ced4519bd2927b6762ca3cc53c7275
dvargas42 <dvargas.eng@gmail.com>
2021-05-30 20:47:23 -0300

    feat: implement data renderization

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,6 +7,7 @@ import {
   Heading,
   Icon,
   IconButton,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -22,8 +23,11 @@ import { Header } from "../../components/Header";
 import { SearchBox } from "../../components/Header/SearchBox";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUser } from "../../hooks/useUsers";
 
 export default function UserList() {
+  const { data, isLoading, isFetching, error } = useUser()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -41,7 +45,11 @@ export default function UserList() {
 
           <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding={["5", "8"]}>
             <Flex marginBottom="8" justifyContent="space-between" alignItems="center">
-              <Heading size="lg" fontWeight="normal">Usuários</Heading>
+              <Heading size="lg" fontWeight="normal">
+                Usuários
+
+                { !isLoading && isFetching && <Spinner size="sm" color="gray.500" marginLeft="4" />}
+              </Heading>
 
               <Link href="/users/create" passHref>
                 <Button
@@ -56,170 +64,76 @@ export default function UserList() {
               </Link>
             </Flex>
 
-            <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th width={isWideVersion ? "8" : "6"} paddingX={isWideVersion ? "6" : "3"} color="gray.300">
-                    <Checkbox colorScheme="pink" />
-                  </Th>
+            { isLoading ? (
+              <Flex justifyContent="center">
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex justifyContent="center">
+                <Text>Falha ao obter dados dos usuários</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme="whiteAlpha">
+                  <Thead>
+                    <Tr>
+                      <Th width={isWideVersion ? "8" : "6"} paddingX={isWideVersion ? "6" : "3"} color="gray.300">
+                        <Checkbox colorScheme="pink" />
+                      </Th>
 
-                  <Th paddingX={isWideVersion ? "6" : "3"} >Usuário</Th>
+                      <Th paddingX={isWideVersion ? "6" : "3"} >Usuário</Th>
 
-                  { isWideVersion && <Th>Data de Cadastro</Th> }
+                      { isWideVersion && <Th>Data de Cadastro</Th> }
 
-                  <Th width={isWideVersion ? "8" : "3"}></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr color="white">
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Vargas</Text>
-                      <Text fontSize={isWideVersion ? "sm" : "sm"} color="gray.300">dvargas.eng@gmail.com</Text>
-                    </Box>
-                  </Td>
-
-                  { isWideVersion && <Td>28 de Maio, 2021</Td> }
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    { isWideVersion ? (
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
-                      ):(
-                      <IconButton
-                        aria-label="Botão editar"
-                        as="a"
-                        size="sm"
-                        colorScheme="purple"
-                        icon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      />
+                      <Th width={isWideVersion ? "8" : "3"}></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {data.map(user => (
+                      <Tr key={user.id} color="white">
+                        <Td paddingX={isWideVersion ? "6" : "3"}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+  
+                        <Td paddingX={isWideVersion ? "6" : "3"}>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize={isWideVersion ? "sm" : "x-small"} color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+  
+                        { isWideVersion && <Td>{user.createdAt}</Td> }
+  
+                        <Td paddingX={isWideVersion ? "6" : "3"}>
+                          { isWideVersion ? (
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                            >
+                              Editar
+                            </Button>
+                            ):(
+                            <IconButton
+                              aria-label="Botão editar"
+                              as="a"
+                              size="sm"
+                              colorScheme="purple"
+                              icon={<Icon as={RiPencilLine} fontSize="16"/>}
+                            />
+                          )}
+                        </Td>
+                      </Tr>
+                    )
                     )}
-                  </Td>
-                </Tr>
-                <Tr color="white">
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
+                  </Tbody>
+                </Table>
 
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Vargas</Text>
-                      <Text fontSize={isWideVersion ? "sm" : "sm"} color="gray.300">dvargas.eng@gmail.com</Text>
-                    </Box>
-                  </Td>
-
-                  { isWideVersion && <Td>28 de Maio, 2021</Td> }
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    { isWideVersion ? (
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
-                      ):(
-                      <IconButton
-                        aria-label="Botão editar"
-                        as="a"
-                        size="sm"
-                        colorScheme="purple"
-                        icon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      />
-                    )}
-                  </Td>
-                </Tr>
-                <Tr color="white">
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Vargas</Text>
-                      <Text fontSize={isWideVersion ? "sm" : "sm"} color="gray.300">dvargas.eng@gmail.com</Text>
-                    </Box>
-                  </Td>
-
-                  { isWideVersion && <Td>28 de Maio, 2021</Td> }
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    { isWideVersion ? (
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
-                      ):(
-                      <IconButton
-                        aria-label="Botão editar"
-                        as="a"
-                        size="sm"
-                        colorScheme="purple"
-                        icon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      />
-                    )}
-                  </Td>
-                </Tr>
-                <Tr color="white">
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    <Box>
-                      <Text fontWeight="bold">Daniel Vargas</Text>
-                      <Text fontSize={isWideVersion ? "sm" : "sm"} color="gray.300">dvargas.eng@gmail.com</Text>
-                    </Box>
-                  </Td>
-
-                  { isWideVersion && <Td>28 de Maio, 2021</Td> }
-
-                  <Td paddingX={isWideVersion ? "6" : "3"}>
-                    { isWideVersion ? (
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      >
-                        Editar
-                      </Button>
-                      ):(
-                      <IconButton
-                        aria-label="Botão editar"
-                        as="a"
-                        size="sm"
-                        colorScheme="purple"
-                        icon={<Icon as={RiPencilLine} fontSize="16"/>}
-                      />
-                    )}
-                  </Td>
-                </Tr>
-                
-              </Tbody>
-            </Table>
-
-            <Pagination />
+                <Pagination />
+              </>
+            )}
           </Box>
         </Box>  
       </Flex>
