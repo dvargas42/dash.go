8367d8ced195b6968ca78d0d31a79c43960b7a44
dvargas42 <dvargas.eng@gmail.com>
2021-05-30 20:50:45 -0300

    feat: implement virtual data server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,13 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { SiderbarDrawerProvider } from '../hooks/SidebarDrawerHook'
+import { makeServer } from '../services/mirage'
 
 import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
