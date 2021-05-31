8ca10a90bb57bb42aafc5c98bea3296829d29ea2
dvargas42 <dvargas.eng@gmail.com>
2021-05-31 18:15:41 -0300

    feat: create pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import faker from 'faker'
 
 type User = {
@@ -33,14 +33,30 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes() {
       this.namespace = 'api'
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, items_per_page = 10 } = request.queryParams
+
+        const totalItems = schema.all('user').length
+
+        const startItem = (Number(page) - 1) * Number(items_per_page)
+        const endItem = startItem + Number(items_per_page)
+      
+        const users = this.serialize(schema.all('user'))
+          .users.slice(startItem, endItem)
+
+        return new Response (
+          200,
+          { 'x-total-count': String(totalItems)},
+          { users }
+        )
+      })
 
       this.post('/users')
 
